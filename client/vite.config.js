7431d7502102b5e2a672cd49f3c4c9d127acb713
git_gael <mr.jgael@gmail.com>
2023-07-11 00:50:04 +0300

    build with minify and splits assets

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,20 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
+  build: {
+    cssMinify: true,
+    minify: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id){
+          if(id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString()
+          }
+        }
+      }
+    }
+  },
+
   server: {
     host: true,
     proxy: {
